7edd6a39be0cee23d0518fc710ea7c639bdf8e8d
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2021-12-24 17:57:16 -0500

    Create challenge in external script to Debug

---
+++ app.js
@@ -0,0 +1,13 @@
+function debughlg(find, num) {
+  let gotMe = 0;
+  if (find === num) {
+    gotMe= find += num
+    console.log(gotMe);
+  } else if 
+  (find > num) {
+    console.log(num);
+  } else 
+  return gotMe
+}
+
+debugChlg(10, 10);
