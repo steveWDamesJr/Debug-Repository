4592dcf3421dcf157e838d543b3785a18cc559f4
IfzaRasool <ifzarasoolarain@gmail.com>
2021-12-25 02:13:49 +0300

    Fix error in script file -Add semi colon -correct function name

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function debughlg(find, num) {
+function debugChlg(find, num) {
   let gotMe = 0;
   if (find === num) {
-    gotMe= find += num
+    gotMe = find += num;
     console.log(gotMe);
   } else if 
   (find > num) {
     console.log(num);
   } else 
-  return gotMe
+  return gotMe;
 }
 
-debugChlg(10, 10);
+ debugChlg(10, 10);
